96ffb83512b7f897fc38a1d84881b3e13bad28b2
mingjaeng <mjkimjeck@naver.com>
2025-05-18 20:52:48 +0900

    Update urls.py

--- 2025-capstone-main/capstone/urls.py
+++ 2025-capstone-main/capstone/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # include 추가
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('capstone.urls')),  # 이 줄 추가: capstone의 urls.py 사용
 ]
