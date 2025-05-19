5057b13dc30d64ee0da7f7cac0d12d5b02ef52a9
mingjaeng <mjkimjeck@naver.com>
2025-05-20 02:29:09 +0900

    Update urls.py

--- 2025-capstone-main/capstone_app/urls.py
+++ 2025-capstone-main/capstone_app/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
-    path('', lambda request: redirect('stock_info'), name='home'),  # 루트 요청을 stock_info로 리디렉션
-    path('stock-info/', views.stock_info, name='stock_info'),
+    path('stock-info/', views.get_stock_info, name='stock_info'),  # ✅ 함수 이름 맞춤
 ]
