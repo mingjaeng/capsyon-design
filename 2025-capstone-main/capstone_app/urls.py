2a76116b6d2bbb282043885de1bfff51942d4959
mingjaeng <mjkimjeck@naver.com>
2025-05-20 02:45:31 +0900

    Update urls.py

--- 2025-capstone-main/capstone_app/urls.py
+++ 2025-capstone-main/capstone_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('stock-info/', views.stock_info_api, name='stock_info'),  # ✅ 올바른 함수 이름
+    path('stock-info/', views.stock_info_api, name='stock_info'),  # API 엔드포인트
 ]
