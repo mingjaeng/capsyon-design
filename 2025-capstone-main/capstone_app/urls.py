0f5f2814f9faab7653ccfb50fd31daa3238bf630
mingjaeng <mjkimjeck@naver.com>
2025-05-20 02:31:33 +0900

    Update urls.py

--- 2025-capstone-main/capstone_app/urls.py
+++ 2025-capstone-main/capstone_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('stock-info/', views.get_stock_info, name='stock_info'),  # ✅ 함수 이름 맞춤
+    path('stock-info/', views.stock_info_api, name='stock_info'),
 ]
